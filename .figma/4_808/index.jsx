e49e2ce767c7eb6f09b24ca63a56cfc8861513ab
longzzx <791087213@qq.com>
2026-03-26 01:20:53 +0800

    chore: 添加Figma导出的图像和组件文件
    添加了Figma设计导出的多个PNG图像文件、对应的React组件及样式文件，用于在项目中集成设计资源。

---
+++ .figma/4_808/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <img src="../image/mn6b7acw-ddh90l7.svg" className={styles.clipPathGroup} />
+  );
+}
+
+export default Component;
